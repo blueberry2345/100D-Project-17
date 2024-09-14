8ea7e686410524cee701dfe7dc9a90a9e9d42b8b
blueberry2345 <102472091+blueberry2345@users.noreply.github.com>
2024-09-15 00:41:44 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
 import os
+from nntplib import decode_header
 from emailer import Emailer
 from helper import Helper
 import imaplib
 
+# Retrieve environment values
 openai_key = os.getenv("API_KEY")
 email = os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
+smtp = os.getenv("SMTP")
+email_port = os.getenv("PORT")
 
-on = True
-
-
+# Create Helper and Emailer objects
 AI_assistant = Helper(openai_key)
+emailer = Emailer(openai_key, email, password, email_port)
 
-# imap login
+# Connect to Email and login.
 imap = imaplib.IMAP4_SSL("imap.gmail.com")
 imap.login(email, password)
+
+# Get the emails from inbox and then
 imap.select("inbox")
 status, messages = imap.select(None, "ALL")
-email_id_list = messages[0].split()
 
-for id in email_id_list:
-    status, data = imap.fetch(id, "(RFC822)")
+# If successful, then get a list of email IDs from inbox
+if status == "OK":
+    email_id_list = messages[0].split()
+    #
+    for id in email_id_list:
+        status, message = imap.fetch(id, "(RFC822)")
+        content = message.get_payload(decode=True).decode()
 
-    for part in data:
-        if isinstance(part, tuple):
-            message = email.message_from_bytes(part[1])
-
-
-
+        # get from
+        sender = message.get("From")
+        # get subject
+        subject = decode_header(message["Subject"])[0]
+        response = AI_assistant.respond(content)
+        emailer.send_email(sender, "RE: {subject}", response)
 
