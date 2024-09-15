ecff431dd70246158c18b40721161653d587520d
blueberry2345 <102472091+blueberry2345@users.noreply.github.com>
2024-09-15 12:58:57 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -21,20 +21,24 @@ imap.login(email, password)
 
 # Get the emails from inbox and then
 imap.select("inbox")
-status, messages = imap.select(None, "ALL")
+list_status, messages = imap.select(None, "ALL")
 
-# If successful, then get a list of email IDs from inbox
-if status == "OK":
+# If successful, then get a list of email IDs from inbox. Otherwise, print error message.
+if list_status == "OK":
     email_id_list = messages[0].split()
-    #
+    # For each email, get the
     for id in email_id_list:
-        status, message = imap.fetch(id, "(RFC822)")
-        content = message.get_payload(decode=True).decode()
+        email_status, message = imap.fetch(id, "(RFC822)")
+        if email_status == "OK":
+            content = message.get_payload(decode=True).decode()
+            sender = message.get("From")
+            subject = decode_header(message["Subject"])[0]
+            # Generate a response to the message and send email
+            response = AI_assistant.respond(content)
+            emailer.send_email(sender, "RE: {subject}", response)
+        else:
+            print(f"ERROR: Email {id} could not be fetched.")
 
-        # get from
-        sender = message.get("From")
-        # get subject
-        subject = decode_header(message["Subject"])[0]
-        response = AI_assistant.respond(content)
-        emailer.send_email(sender, "RE: {subject}", response)
 
+else:
+    print("ERROR: Inbox Emails could not be retrieved.")
